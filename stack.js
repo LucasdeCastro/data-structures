737e14d94a173d918440859b883a7aaa7c3520c6
lucas de castro <lucascastro102@gmail.com>
2016-03-12 23:04:53 -0300

    Create stack

---
+++ stack.js
@@ -0,0 +1,35 @@
+function Stack(){
+    var self = this;
+    self.stack = null;
+
+    // Element Object
+    function Element(value, prev){
+        this.value = value;
+        this.prev  = prev;
+    };
+
+    // Remove top element
+    self.pop = function(){
+        self.stack = self.stack.prev;
+    };
+
+    // Add element
+    self.add = function(value){
+        self.stack = new Element(value, self.stack);
+    };
+
+    // Check is empty
+    self.isEmpty = function(){
+        return self.stack == null;
+    };
+
+    // Clear stack
+    self.clear = function(){
+        self.stack = null;
+    };
+
+    // Return Top element
+    self.topEl = function(){
+        return self.stack.value;
+    };
+};
